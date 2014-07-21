3d95732b045bfe44a16c150598d73009b12fd845
Jeremy Burns <jeremyaburns@gmail.com>
2014-07-20 23:58:25 -0600

    Cleaned up index.js

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
 'use strict';
 
-var fs = require('fs');
+var CLIENT_SCRIPT_PATH = '/gamesync-client.js';
+var CLIENT_SCRIPT_URL = '/gamesync/client.js'
+
+// ----------------------------------------------------------------------------
+
+var clientScript = require('fs').readFileSync(__dirname+CLIENT_SCRIPT_PATH).toString();
 var GameRunner = require('./gamerunner');
 
-var clientJS;
+function addPriorityRequestListener (server, url, listener) {
+  var oldListeners = server.listeners('request').splice(0);
+  server.removeAllListeners('request');
 
-fs.readFile (__dirname+'/gamesync-client.js', function (err, data) {
-    if (err) {
-        console.log ('Could not find gamesync-client.js');
-        process.exit (1);
+  server.on('request', function (req, res) {
+    if (req.url === url) {
+      listener (req, res);
     }
-    clientJS = data;
-});
+    else {
+      for (var i=0, l=oldListeners.length; i < l; i++) {
+        oldListeners[i].call (server, req, res);
+      }
+    }
+  });
+}
 
-exports.run = function (io, game, lag)
-{
-    if (typeof lag === 'undefined') lag = 0;
+module.exports.run = function (io, game, lag) {
+  if (typeof lag === 'undefined') lag = 0;
 
-    var oldListeners = io.server.listeners('request').splice(0);
-    io.server.removeAllListeners('request');
+  addPriorityRequestListener (io.server, CLIENT_SCRIPT_URL, function (req, res) {
+    res.writeHead (200);
+    res.end (clientJS);
+  });
 
-    io.server.on('request', function (req, res) {
-        if (req.url === '/gamesync/client.js') {
-            res.writeHead (200);
-            res.end (clientJS);
-        }
-        else {
-            for (var i=0, l=oldListeners.length; i < l; i++) {
-                oldListeners[i].call (io.server, req, res);
-            }
-        }
+  var gameRunner = new GameRunner (game, lag);
+
+  io.sockets.on ('connection', function (socket) {
+    var client = gameRunner.addClientSocket (socket);
+
+    if (!client) {
+      socket.json.send ({'error': 'Too many players connected already!'});
+      return;
+    }
+
+    socket.json.send ({'notifyInputId': client.inputId});
+
+    socket.on ('disconnect', function () {
+      gameRunner.removeClientSocket (socket);
     });
 
-    var gameRunner = new GameRunner (game, lag);
-
-    io.sockets.on ('connection', function (socket) {
-        var client = gameRunner.addClientSocket (socket);
-
-        if (!client) {
-            socket.json.send ({'error': 'Too many players connected already!'});
-            return;
-        }
-
-        socket.json.send ({'notifyInputId': client.inputId});
-
-        socket.on ('disconnect', function () {
-            gameRunner.removeClientSocket (socket);
-        });
-
-        socket.on ('message', function (data) {
-            client.acceptInput (data.ackId, data.frame, data.input);
-        });
+    socket.on ('message', function (data) {
+      client.acceptInput (data.ackId, data.frame, data.input);
     });
+  });
 }
 
 
