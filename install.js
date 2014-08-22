d76d9f744ed9c744850cfac4daf15e6e683ad573
Jeremy Burns <jeremyaburns@gmail.com>
2014-08-22 15:15:40 -0600

    Removed check for exist in install.js

--- install.js
+++ install.js
@@ -1,12 +1,13 @@
 'use strict';
 
 var fs = require('fs');
+var browserify = require('browserify');
 
-// Build the client bundle using browserify if it doesn't exist.
-if (! fs.existsSync('client.bundle.js')) {
-  var b = require('browserify')('./client/main');
-  b.bundle(function(err,buf) {
-    if (err) console.error(err);
-    else fs.writeFileSync('client.bundle.js', buf.toString());
-  });
-}
+browserify('./client/main').bundle(function(err,buf) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  fs.writeFileSync('client.bundle.js', buf.toString());
+});
